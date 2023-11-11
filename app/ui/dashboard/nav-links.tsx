2ed81621a4a6d619a1dd61ccca83f6aaf5995ad3
UnclePacky <claudeaoun@gmail.com>
2023-11-11 18:33:20 +0200

    Correct test

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,7 +11,7 @@ import { usePathname } from "next/navigation";
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: "Homes", href: "/dashboard", icon: HomeIcon },
+  { name: "Home", href: "/dashboard", icon: HomeIcon },
   {
     name: "Invoices",
     href: "/dashboard/invoices",
